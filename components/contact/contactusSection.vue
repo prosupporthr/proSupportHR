f6d06055f9c494f009992d991de4a8b578943d28
ekadaegile <egileoniso.ekada@gmail.com>
2025-04-25 14:53:19 +0100

    update

--- components/contact/contactusSection.vue
+++ components/contact/contactusSection.vue
@@ -14,6 +14,10 @@
                     <div class=" w-full flex gap-1 flex-col " >
                         <p class=" font-medium text-primary-text " >Email</p> 
                         <input class=" w-full h-[52px] border border-border-color px-3 rounded-lg " />
+                    </div> 
+                    <div class=" w-full flex gap-1 flex-col " >
+                        <p class=" font-medium text-primary-text " >Phone Number (Optional)</p> 
+                        <input class=" w-full h-[52px] border border-border-color px-3 rounded-lg " />
                     </div>
                     <button class=" w-full h-[56px] bg-blue-bg rounded-full text-white font-semibold mt-4 " >Send</button>
                 </div>
--- components/home/ourServiceSection.vue
+++ components/home/ourServiceSection.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-    import ServiceCard from '../components/shared/serviceCard.vue' 
+    import ServiceCard from '../shared/serviceCard.vue' 
  
     import { ref, onMounted } from 'vue'
     import { gsap } from "gsap"
@@ -21,7 +21,7 @@
                     delay: index * 0.15,
                     scrollTrigger: {
                         trigger: boxRef.value,
-                        start: "top 80%"
+                        start: "top 40%"
                     }
                 })
             } else if (index === 1){
@@ -32,7 +32,7 @@
                     delay: index * 0.15,
                     scrollTrigger: {
                         trigger: boxRef.value,
-                        start: "top 80%"
+                        start: "top 40%"
                     }
                 })
             }  
@@ -41,7 +41,11 @@
 </script>
 
 <template>
-    <div class=" lg:py-20 w-full flex justify-center lg:px-0 px-3 " >
+    <div class=" lg:py-20 w-full flex justify-center lg:px-0 px-3 relative  " >
+        <div class=" absolute inset-0 flex flex-col " > 
+            <img src="/images/aboutforebg.png" alt="forbg" class=" w-full object-cover h-full  " /> 
+            <img src="/images/aboutforebg.png" alt="forbg" class=" w-full object-cover lg:block hidden h-full  " />
+        </div>
         <div class=" w-full lg:w-[85%] flex flex-col items-center gap-1 " >
             <div ref="box1" class=" flex flex-col gap-1 items-center " > 
                 <p class=" font-semibold text-[30px] lg:text-[44px] text-primary-text " >Our Services</p>
@@ -54,10 +58,9 @@
                     <ServiceCard />
                     <ServiceCard />
                     <ServiceCard />
-                    <ServiceCard />
-                    <ServiceCard />
+                    <ServiceCard /> 
                 </div>
-                <div  class="w-full lg:hidden grid grid-cols-1 lg:grid-cols-3 gap-4 mt-12 " >
+                <div class="w-full lg:hidden grid grid-cols-1 lg:grid-cols-3 gap-4 mt-12 " >
                     <ServiceCard />
                     <ServiceCard />
                     <ServiceCard /> 
