e293fd4eaba8511380165346f267463986a59089
neon <danielemmanuel257@gmail.com>
2025-05-01 21:57:24 +0100

    added filters

--- server/utils/extractQuery.ts
+++ server/utils/extractQuery.ts
@@ -1,10 +1,16 @@
-const extractQuery = (query?: { page?: string, limit?: string}) => {
-    const obj: { page: number, limit: number } = {
+const extractQuery = (query?: { page?: string, limit?: string, category: string | undefined, price: string | undefined, title: string | undefined }) => {
+    const obj: { page: number, limit: number, category: string | undefined, price: number | undefined, title: string | undefined } = {
         page: 1,
         limit: 10,
+        category: undefined,
+        price: undefined,
+        title: undefined,
     };
     obj['page'] = query?.page ? parseInt(query?.page) : 1;
     obj['limit'] = query?.limit ? parseInt(query?.limit) : 10;
+    obj['category'] = query?.category ? query?.category : undefined;
+    obj['price'] = query?.price ? parseInt(query?.price) : undefined;
+    obj['title'] = query?.title ? query?.title : undefined;
 
     return obj;
 }
