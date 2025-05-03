56fdf469834486decba0f98ce5a8395e2d42c858
ekadaegile <egileoniso.ekada@gmail.com>
2025-05-03 21:18:08 +0100

    update

--- components/shared/serviceCard.vue
+++ components/shared/serviceCard.vue
@@ -1,15 +1,22 @@
 <template> 
-    <div class=" w-full flex-col flex rounded-t-2xl rounded-b-xl " >
+    <div class=" w-full flex-col h-full flex rounded-t-2xl rounded-b-xl " >
         <div class=" w-full h-[100px] lg:h-[156px] rounded-t-2xl " >
             <img src="/images/service.png" class=" object-cover w-full h-full rounded-t-2xl " />
         </div>  
-        <div class=" p-2 lg:p-4 w-full flex gap-3 flex-col border-t-0 border-[0.36px] border-border-color bg-white rounded-b-xl " >
-            <p class=" text-sm lg:text-2xl font-bold text-fondation-text-500 " >Performance Management</p>
-            <ul class=" flex flex-col gap-2 " >
-                <li class=" text-secondary lg:text-base text-xs " >✔ Create structured goal-setting and performance evaluation systems.</li>
-                <li class=" text-secondary lg:text-base text-xs " >✔ Implement employee development plans that align with business growth.</li>
-                <li class=" text-secondary lg:text-base text-xs " >✔ Provide tools and frameworks for tracking performance and boosting productivity.</li>
-            </ul>
+        <div class=" p-2 lg:p-4 w-full h-full flex gap-3 flex-col border-t-0 border-[0.36px] border-border-color bg-white rounded-b-xl " >
+            <p class=" text-sm lg:text-2xl font-bold text-fondation-text-500 " >{{ props?.item?.title }}</p>
+            <p class=" text-secondary lg:text-base text-xs ">{{item?.body}}</p>
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+ 
+    const props = defineProps<{
+        item: {
+            title: string,
+            body: string
+        };
+    }>();
+
+</script>
--- pages/services.vue
+++ pages/services.vue
@@ -3,6 +3,7 @@
 import ServiceHeroSection from '../components/service/serviceHeroSection.vue'
 import ServiceCard from '../components/shared/serviceCard.vue'
 import NeedHelpSection from '../components/shared/needHelpSection.vue' 
+import { ServiceData } from "../assets/databank/services"
 
 definePageMeta({
   layout: 'default',
@@ -13,10 +14,11 @@ definePageMeta({
     <ServiceHeroSection />
     <div class=" w-full flex flex-col items-center lg:py-20 lg:gap-0 gap-6 ">
       <div class=" lg:px-0 px-3 lg:max-w-[85%] w-full flex flex-col gap-6 ">
-        <p class=" text-3xl lg:text-[32px] text-black max-w-[950px] ">Aligning Services with Our Vision to Empower Small
-          Businesses Through HR Compliance and Process Excellence</p>
+        <p class=" text-3xl lg:text-[32px] text-black max-w-[950px] ">Aligning Services with Our Vision to Empower Small Businesses Through HR Compliance and Process Excellence</p>
         <div class="w-full grid grid-cols-1 lg:grid-cols-3 gap-2 lg:gap-4 lg:mt-10 ">
-          <ServiceCard />
+          <div v-for="item in ServiceData" :key="item.title" >
+            <ServiceCard :item='item' />
+          </div>
         </div>
       </div>
       <NeedHelpSection />
