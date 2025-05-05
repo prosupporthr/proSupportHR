11a908350d1e077989d06dc5de6d0f9d4def5ace
ekadaegile <egileoniso.ekada@gmail.com>
2025-05-05 15:01:45 +0100

    update on design

--- components/contact/contactusSection.vue
+++ components/contact/contactusSection.vue
@@ -1,15 +1,19 @@
 <template>
     <div class=" w-full flex justify-center lg:pb-20 px-3 " >   
         <div class=" w-full lg:w-[85%] py-12 flex flex-col items-center shadow-2xl rounded-2xl gap-4 " >  
-            <div class=" w-full flex gap-6 justify-center " >   
-                <UIcon name="i-lucide-facebook" class="size-7 text-[#0890FF] " />
-                <UIcon name="i-lucide-twitter" class="size-7 text-[#0890FF] " />
-                <UIcon name="i-lucide-linkedin" class="size-5 text-[#0890FF] " />
-                <UIcon name="i-lucide-instagram" class="size-5 text-[#0890FF] " />
-                <UIcon name="i-lucide-youtube" class="size-5 text-[#0890FF] " />
+            <div class=" w-full flex gap-6 justify-center " >    
+                <NuxtLink target="_blank" to="https://www.facebook.com/share/1581Vrk6Pp/?mibextid=wwXIfr" >
+                    <UIcon name="i-lucide-facebook" class="size-7 text-[#0890FF] " />
+                </NuxtLink> 
+                <NuxtLink target="_blank" to="https://www.linkedin.com/company/prosupport-hr-partners" >
+                    <UIcon name="i-lucide-linkedin" class="size-5 text-[#0890FF] " />
+                </NuxtLink>
+                <NuxtLink target="_blank" to="https://www.instagram.com/prosupporthr/profilecard/?igsh=ZDY1OWw1N3ZiNHQ3" >
+                    <UIcon name="i-lucide-instagram" class="size-5 text-[#0890FF] " />
+                </NuxtLink> 
             </div>
             <p class=" text-base-color font-medium " >Send us a dm on various Social handles</p>
-            <button class=" w-[174px]  text-white font-semibold text-sm h-[50px] rounded-4xl bg-blue-bg " >Get in Touch</button>
+            <!-- <button class=" w-[174px]  text-white font-semibold text-sm h-[50px] rounded-4xl bg-blue-bg " >Get in Touch</button> -->
         </div>
     </div>
 </template>
