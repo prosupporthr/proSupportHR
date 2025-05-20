498ed42b8da474b71c49393dfd249e0ed0b1ac92
egileekada <egileoniso.ekada@gmail.com>
2025-05-20 19:54:34 +0100

    update on image hero section

--- components/shop/shopHeroSection.vue
+++ components/shop/shopHeroSection.vue
@@ -48,11 +48,11 @@
             <Navbar />
         </div> -->
         <div class=" w-full h-full relative " >
-            <img src="/images/shop.jpeg" alt="servicebg" class=" w-full  h-[60vh] lg:h-[80vh] object-cover " />
+            <img src="/images/shop.jpg" alt="servicebg" class=" w-full  h-[60vh] lg:h-[80vh] object-cover " />
             <div ref="box1" class=" absolute inset-x-0 bottom-0 top-[120px] z-20 flex items-center justify-center " >
                 <div class=" lg:w-[85%] lg:px-0 px-3 h-full flex justify-center pb-12 items-center flex-col gap-2 text-white " >  
-                    <p v-if="searchTerm" class=" font-bold max-w-[700px] lg:text-center lg:leading-[50.4px] leading-[45px] text-[40px] lg:text-[50px] text-white " >{{ searchTerm.toString().replaceAll('-', ' ') }}</p>
-                    <p v-if="!searchTerm" class=" font-bold max-w-[700px] lg:text-center lg:leading-[50.4px] leading-[45px] text-[40px] lg:text-[50px] text-white " >HRGenie - The HR Compliance Toolkit</p>
+                    <p v-if="searchTerm" class=" font-bold max-w-[700px] lg:text-center leading-[120%] text-[40px] lg:text-[50px] text-white " >{{ searchTerm.toString().replaceAll('-', ' ') }}</p>
+                    <p v-if="!searchTerm" class=" font-bold max-w-[700px] lg:text-center leading-[120%] text-[40px] lg:text-[50px] text-white " >HRGenie - The HR Compliance Toolkit</p>
                     <p class=" text-white " >Get ready-to-use HR templates for your business! Download instantly after purchase.</p>
                 </div>
             </div>
