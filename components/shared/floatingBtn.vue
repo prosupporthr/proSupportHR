409c55c37ca17fe9cfe1b452d863bd7b278d5ac7
egileekada <egileoniso.ekada@gmail.com>
2025-05-22 14:48:08 +0100

    update

---
+++ components/shared/floatingBtn.vue
@@ -0,0 +1,16 @@
+<template>
+    <div :class="route.path === '/shop' ? 'hidden' : 'fixed bottom-6 z-50 right-6 m-4 ' " class=" fixed bottom-6 z-50 right-6 m-4 ">
+        <UButton @click="goToSection" class=" bg-blue-bg px-3 py-2 h-fit cursor-pointer rounded-lg text-white " >
+            Shop Now
+        </UButton>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const router = useRouter();
+    const route = useRoute();
+
+    const goToSection = () => {
+        router.push(`/shop`);
+    };
+</script>
