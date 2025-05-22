3db406f14a87105ababa710219be8e09b1a6d133
egileekada <egileoniso.ekada@gmail.com>
2025-05-22 14:49:46 +0100

    update

--- components/shared/floatingBtn.vue
+++ components/shared/floatingBtn.vue
@@ -1,6 +1,6 @@
 <template>
     <div :class="route.path === '/shop' ? 'hidden' : 'fixed bottom-6 z-50 right-6 m-4 ' " class=" fixed bottom-6 z-50 right-6 m-4 ">
-        <UButton @click="goToSection" class=" bg-blue-bg px-3 py-2 h-fit cursor-pointer rounded-lg text-white " >
+        <UButton @click="goToSection" class=" bg-blue-bg px-3 py-2 h-fit cursor-pointer rounded-full text-white " >
             Shop Now
         </UButton>
     </div>
