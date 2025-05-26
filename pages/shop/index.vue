96cebb6145c0cb7fe18f37d338db25cdc53f1b47
egileekada <egileoniso.ekada@gmail.com>
2025-05-26 16:47:34 +0100

    update ui

--- pages/shop/index.vue
+++ pages/shop/index.vue
@@ -23,13 +23,13 @@ useHead({
         </div>
         <div class=" w-full grid grid-cols-2 lg:grid-cols-3 gap-2 lg:gap-4">
           <div 
-            v-for="item in shopcategories" @click="router?.push(`/shop/category?key=${encodeURIComponent(item?.name.toString())}`)" class=" w-full p-2 rounded-2xl cursor-pointer flex gap-2 flex-col ">
+            v-for="item in shopcategories" @click="router?.push(`/shop/category?key=${encodeURIComponent(item?.name.toString())}`)" class=" w-full h-full p-2 rounded-2xl cursor-pointer flex gap-2 flex-col ">
             <div class=" w-[60px] h-[70px]" >
               <img :src="item?.image" alt="image" class=" w-full " />
             </div>
             <p class=" text-lg font-bold " >{{ item?.name }}</p>
             <p class=" text-[#667085] " >{{ item?.detail }}</p>
-            <div class=" mt-4 flex gap-2 items-center " >  
+            <div class=" pt-4 mt-auto flex gap-2 items-center " >  
               <p class=" font-medium text-[#0890FF] " >View Template</p>
               <UIcon name="i-lucide-arrow-right" class="size-7 mt-[1px] text-[#0890FF] " />
             </div> 
